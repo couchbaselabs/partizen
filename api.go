0cf43989a51d7cbe7aaaaeb80f6e3e5d785f7952
Steve Yen <steve.yen@gmail.com>
2014-11-14 19:13:12 -0800

    reordered API so it's more top-down

--- api.go
+++ api.go
@@ -10,34 +10,16 @@ type Val []byte
 type PartitionID uint16
 type Seq uint64
 
+func Open(sf StoreFile, options StoreOptions) (Store, error) {
+	return nil, nil
+}
+
 type StoreFile interface {
 	io.ReaderAt
 	io.WriterAt
 	Stat() (os.FileInfo, error)
 	Truncate(size int64) error
 }
-
-func Open(sf StoreFile, options StoreOptions) (Store, error) {
-	return nil, nil
-}
-
-type StoreOptions struct {
-	CompareFuncs map[string]CompareFunc // Keyed by compareFuncName.
-
-	BufAlloc   func(size int) []byte
-	BufAddRef  func(buf []byte)
-	BufDecRef  func(buf []byte)
-	BufGetNext func(buf []byte) (bufNext []byte)
-	BufSetNext func(buf, bufNext []byte)
-}
-
-// A CompareFunc should return 0 if a == b, -1 if a < b, and +1 if a >
-// b. For example: bytes.Compare()
-type CompareFunc func(a, b []byte) int
-
-type ChangeStats struct{}
-
-type StoreStats struct{}
 
 type Store interface {
 	CommitChanges() (ChangeStats, error)
@@ -57,37 +39,31 @@ type Store interface {
 	Stats(dest *StoreStats) error
 }
 
-type VisitorFunc func(partition PartitionID, key []byte, seq Seq, value []byte) bool
-
-type MergeFunc func(base, a, b []byte) ([]byte, error)
-
 type Collection interface {
-	CompareFunc() CompareFunc
-
 	Get(partition PartitionID,
-		key []byte,
+		key Key,
 		withValue bool, // When withValue is false, value will be nil.
 		fastSample bool) ( // Return result only if fast / in memory (no disk hit).
-		seq Seq, value []byte, err error)
+		seq Seq, val Val, err error)
 
 	// Set takes a seq number that should be monotonically increasing.
-	Set(partition PartitionID, key []byte, seq Seq, value []byte) error
+	Set(partition PartitionID, key Key, seq Seq, val Val) error
 
 	// Merge takes a seq number that should be monotonically increasing.
-	Merge(partition PartitionID, key []byte, seq Seq, mergeFunc MergeFunc) error
+	Merge(partition PartitionID, key Key, seq Seq, mergeFunc MergeFunc) error
 
 	// Del takes a seq number that should be monotonically increasing.
-	Del(partition PartitionID, key []byte, seq Seq) error
+	Del(partition PartitionID, key Key, seq Seq) error
 
 	Min(withValue bool) (
-		partition PartitionID, key []byte, seq Seq, value []byte, err error)
+		partition PartitionID, key Key, seq Seq, val Val, err error)
 	Max(withValue bool) (
-		partition PartitionID, key []byte, seq Seq, value []byte, err error)
+		partition PartitionID, key Key, seq Seq, val Val, err error)
 
 	// Scan provides range results in [fromKeyInclusive...toKeyExclusive) sequence,
 	// even when the reverse flag is true.
-	Scan(fromKeyInclusive []byte,
-		toKeyExclusive []byte,
+	Scan(fromKeyInclusive Key,
+		toKeyExclusive Key,
 		reverse bool, // When reverse flag is true, fromKey should be greater than toKey.
 		partitions []PartitionID, // Focus on subset of partitions; nil for all partitions.
 		withValue bool, // When withValue is false, value will be nil.
@@ -107,3 +83,26 @@ type Collection interface {
 	// seq number.
 	Rollback(partition PartitionID, seq Seq, exact bool) error
 }
+
+type StoreOptions struct {
+	CompareFuncs map[string]CompareFunc // Keyed by compareFuncName.
+
+	BufAlloc   func(size int) []byte
+	BufAddRef  func(buf []byte)
+	BufDecRef  func(buf []byte)
+	BufGetNext func(buf []byte) (bufNext []byte)
+	BufSetNext func(buf, bufNext []byte)
+}
+
+type VisitorFunc func(partition PartitionID, key Key, seq Seq, val Val) bool
+
+type MergeFunc func(base, a, b []byte) ([]byte, error)
+
+// A CompareFunc should return 0 if a == b, -1 if a < b,
+// and +1 if a > b.  For example: bytes.Compare()
+type CompareFunc func(a, b []byte) int
+
+type ChangeStats struct{}
+
+type StoreStats struct{}
+
