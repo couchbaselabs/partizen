20717a53109f2b7dc0a29232148d2569fe0929fb
Steve Yen <steve.yen@gmail.com>
2014-11-18 00:08:45 -0800

    rename RootLoc var to r

--- coll.go
+++ coll.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-func (c *RootLoc) Get(partitionID PartitionID, key Key) (
+func (r *RootLoc) Get(partitionID PartitionID, key Key) (
 	seq Seq, val Val, err error) {
-	return c.GetEx(partitionID, key, true, false)
+	return r.GetEx(partitionID, key, true, false)
 }
 
-func (c *RootLoc) GetEx(partitionID PartitionID,
+func (r *RootLoc) GetEx(partitionID PartitionID,
 	key Key,
 	withValue bool, // When withValue is false, value will be nil.
 	fastSample bool) ( // Return result only if fast / in memory (no disk hit).
@@ -17,31 +17,31 @@ func (c *RootLoc) GetEx(partitionID PartitionID,
 	return 0, nil, fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Set(partitionID PartitionID, key Key, seq Seq,
+func (r *RootLoc) Set(partitionID PartitionID, key Key, seq Seq,
 	val Val) error {
 	return fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Merge(partitionID PartitionID, key Key, seq Seq,
+func (r *RootLoc) Merge(partitionID PartitionID, key Key, seq Seq,
 	mergeFunc MergeFunc) error {
 	return fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Del(partitionID PartitionID, key Key, seq Seq) error {
+func (r *RootLoc) Del(partitionID PartitionID, key Key, seq Seq) error {
 	return fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Min(withValue bool) (
+func (r *RootLoc) Min(withValue bool) (
 	partitionID PartitionID, key Key, seq Seq, val Val, err error) {
 	return 0, nil, 0, nil, fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Max(withValue bool) (
+func (r *RootLoc) Max(withValue bool) (
 	partitionID PartitionID, key Key, seq Seq, val Val, err error) {
 	return 0, nil, 0, nil, fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Scan(fromKeyInclusive Key,
+func (r *RootLoc) Scan(fromKeyInclusive Key,
 	toKeyExclusive Key,
 	reverse bool, // When reverse flag is true, fromKey should be greater than toKey.
 	partitions []PartitionID, // Scan only these partitions; nil for all partitions.
@@ -51,14 +51,14 @@ func (c *RootLoc) Scan(fromKeyInclusive Key,
 	return fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Diff(partitionID PartitionID,
+func (r *RootLoc) Diff(partitionID PartitionID,
 	fromSeqExclusive Seq, // Should be a Seq at some past commit point.
 	withValue bool, // When withValue is false, nil value is passed to visitorFunc.
 	visitorFunc VisitorFunc) error {
 	return fmt.Errorf("unimplemented")
 }
 
-func (c *RootLoc) Rollback(partitionID PartitionID, seq Seq,
+func (r *RootLoc) Rollback(partitionID PartitionID, seq Seq,
 	exactToSeq bool) error {
 	return fmt.Errorf("unimplemented")
 }
