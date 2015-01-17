641746481f1398f608436f212fc81d4ccf347488
Steve Yen <steve.yen@gmail.com>
2015-01-17 11:56:34 -0800

    test more deletes of missing keys in 1 node tree

--- node_mutate_test.go
+++ node_mutate_test.go
@@ -104,37 +104,42 @@ func Test1Update(t *testing.T) {
 		t.Errorf("expected val child is A")
 	}
 
-	m = []Mutation{Mutation{
-		Key: []byte("x"), // DELETE of key that's not in the tree.
-		Op:  MUTATION_OP_DELETE,
-	}}
-	kl2, err := rootNodeLocProcessMutations(&kl.Loc, m, 32, nil)
-	if err != nil {
+	// Try some DELETE's of key that's not in the tree.
+	for _, keyNotThere := range []string{"x", "0", "aa", ""} {
+		m = []Mutation{
+			Mutation{
+				Key: []byte(keyNotThere),
+				Op:  MUTATION_OP_DELETE,
+			},
+		}
+		kl2, err := rootNodeLocProcessMutations(&kl.Loc, m, 32, nil)
+		if err != nil {
 		t.Errorf("expected ok, err: %#v", err)
-	}
-	if kl2 == nil {
-		t.Errorf("expected kl2")
-	}
-	if string(kl2.Key) != "a" {
-		t.Errorf("expected a keyLoc with a Key")
-	}
-	if !isSomeMemLoc(&kl2.Loc, LocTypeNode) {
-		t.Errorf("expected some keyLoc")
-	}
-	if kl2.Loc.node == nil || kl2.Loc.buf != nil {
-		t.Errorf("expected a keyLoc with node, no buf")
-	}
-	if len(kl2.Loc.node.(*NodeMem).KeyLocs) != 1 {
-		t.Errorf("expected 1 child")
-	}
-	if string(kl2.Loc.node.(*NodeMem).KeyLocs[0].Key) != "a" {
-		t.Errorf("expected 1 child")
-	}
-	if !isSomeMemLoc(&kl2.Loc.node.(*NodeMem).KeyLocs[0].Loc, LocTypeVal) {
-		t.Errorf("expected val child")
-	}
-	if string(kl2.Loc.node.(*NodeMem).KeyLocs[0].Loc.buf) != "A" {
-		t.Errorf("expected val child is A")
+		}
+		if kl2 == nil {
+			t.Errorf("expected kl2")
+		}
+		if string(kl2.Key) != "a" {
+			t.Errorf("expected a keyLoc with a Key")
+		}
+		if !isSomeMemLoc(&kl2.Loc, LocTypeNode) {
+			t.Errorf("expected some keyLoc")
+		}
+		if kl2.Loc.node == nil || kl2.Loc.buf != nil {
+			t.Errorf("expected a keyLoc with node, no buf")
+		}
+		if len(kl2.Loc.node.(*NodeMem).KeyLocs) != 1 {
+			t.Errorf("expected 1 child")
+		}
+		if string(kl2.Loc.node.(*NodeMem).KeyLocs[0].Key) != "a" {
+			t.Errorf("expected 1 child")
+		}
+		if !isSomeMemLoc(&kl2.Loc.node.(*NodeMem).KeyLocs[0].Loc, LocTypeVal) {
+			t.Errorf("expected val child")
+		}
+		if string(kl2.Loc.node.(*NodeMem).KeyLocs[0].Loc.buf) != "A" {
+			t.Errorf("expected val child is A")
+		}
 	}
 }
 
