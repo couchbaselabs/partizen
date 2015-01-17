63770135324a67748c8b6a41660880f5eaf4306d
Steve Yen <steve.yen@gmail.com>
2015-01-17 10:22:22 -0800

    added Test2Update()

--- node_mutate_test.go
+++ node_mutate_test.go
@@ -92,3 +92,55 @@ func Test1Update(t *testing.T) {
 		t.Errorf("expected val child is A")
 	}
 }
+
+func Test2Update(t *testing.T) {
+	m := []Mutation{
+		Mutation{
+			Key: []byte("a"),
+			Val: []byte("A"),
+			Op: MUTATION_OP_UPDATE,
+		},
+		Mutation{
+			Key: []byte("b"),
+			Val: []byte("B"),
+			Op: MUTATION_OP_UPDATE,
+		},
+	}
+	kl, err := rootNodeLocProcessMutations(nil, m, 32, nil)
+	if err != nil {
+		t.Errorf("expected ok, err: %#v", err)
+	}
+	if kl == nil {
+		t.Errorf("expected a keyloc")
+	}
+	if string(kl.Key) != "a" {
+		t.Errorf("expected a keyLoc with a Key")
+	}
+	if !isSomeMemLoc(&kl.Loc, LocTypeNode) {
+		t.Errorf("expected some keyLoc")
+	}
+	if kl.Loc.node == nil || kl.Loc.buf != nil {
+		t.Errorf("expected a keyLoc with node, no buf")
+	}
+	if len(kl.Loc.node.(*NodeMem).KeyLocs) != 2 {
+		t.Errorf("expected 2 child")
+	}
+	if string(kl.Loc.node.(*NodeMem).KeyLocs[0].Key) != "a" {
+		t.Errorf("expected child 0 is a")
+	}
+	if !isSomeMemLoc(&kl.Loc.node.(*NodeMem).KeyLocs[0].Loc, LocTypeVal) {
+		t.Errorf("expected val child")
+	}
+	if string(kl.Loc.node.(*NodeMem).KeyLocs[0].Loc.buf) != "A" {
+		t.Errorf("expected val child is A")
+	}
+	if string(kl.Loc.node.(*NodeMem).KeyLocs[1].Key) != "b" {
+		t.Errorf("expected child 1 is b")
+	}
+	if !isSomeMemLoc(&kl.Loc.node.(*NodeMem).KeyLocs[1].Loc, LocTypeVal) {
+		t.Errorf("expected val child")
+	}
+	if string(kl.Loc.node.(*NodeMem).KeyLocs[1].Loc.buf) != "B" {
+		t.Errorf("expected val child is B")
+	}
+}
