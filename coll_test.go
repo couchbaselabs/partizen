43da8ab6bfd91f0df387313ce0784b36dc62fc89
Steve Yen <steve.yen@gmail.com>
2015-01-24 09:32:04 -0800

    more Get/Set tests

--- coll_test.go
+++ coll_test.go
@@ -33,4 +33,45 @@ func TestSimpleMemColl(t *testing.T) {
 	if err != nil || seq != 0 || val != nil {
 		t.Errorf("expected Get on missing key to be empty")
 	}
+	err = c.Set(0, []byte("b"), 2, []byte("B"))
+	if err != nil {
+		t.Errorf("expected Set on 1 item coll to work")
+	}
+	seq, val, err = c.Get(0, []byte("a"), true)
+	if err != nil || seq != 1 || string(val) != "A" {
+		t.Errorf("expected Get(a) to work, got seq: %d, val: %s, err: %v",
+			seq, val, err)
+	}
+	seq, val, err = c.Get(0, []byte("b"), true)
+	if err != nil || seq != 2 || string(val) != "B" {
+		t.Errorf("expected Get(b) to work, got seq: %d, val: %s, err: %v",
+			seq, val, err)
+	}
+	seq, val, err = c.Get(0, []byte("not-there"), true)
+	if err != nil || seq != 0 || val != nil {
+		t.Errorf("expected Get on missing key to be empty")
+	}
+	err = c.Set(0, []byte("0"), 3, []byte("00"))
+	if err != nil {
+		t.Errorf("expected Set on 2 item coll to work")
+	}
+	seq, val, err = c.Get(0, []byte("a"), true)
+	if err != nil || seq != 1 || string(val) != "A" {
+		t.Errorf("expected Get(a) to work, got seq: %d, val: %s, err: %v",
+			seq, val, err)
+	}
+	seq, val, err = c.Get(0, []byte("b"), true)
+	if err != nil || seq != 2 || string(val) != "B" {
+		t.Errorf("expected Get(b) to work, got seq: %d, val: %s, err: %v",
+			seq, val, err)
+	}
+	seq, val, err = c.Get(0, []byte("0"), true)
+	if err != nil || seq != 3 || string(val) != "00" {
+		t.Errorf("expected Get(0) to work, got seq: %d, val: %s, err: %v",
+			seq, val, err)
+	}
+	seq, val, err = c.Get(0, []byte("not-there"), true)
+	if err != nil || seq != 0 || val != nil {
+		t.Errorf("expected Get on missing key to be empty")
+	}
 }
