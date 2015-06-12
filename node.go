2d52bdcc0eb06f5c8b6bf7ac1a60e7c80fe7abd7
Steve Yen <steve.yen@gmail.com>
2015-06-12 11:05:20 -0700

    rm node_io.go

--- node.go
+++ node.go
@@ -23,6 +23,43 @@ func (n *NodeMem) GetPartitions() *Partitions {
 
 // ----------------------------------------------------
 
+func (loc *Loc) Read(bufManager BufManager, r io.ReaderAt) (
+	*Loc, error) {
+	// TODO: Need locking for swizzling.
+
+	if loc.Type == LocTypeNode {
+		if loc.node == nil {
+			return nil, fmt.Errorf("node_io: Read loc type node TODO")
+		}
+		return loc, nil
+	}
+
+	if loc.Type == LocTypeVal {
+		if loc.bufRef.IsNil() {
+			return nil, fmt.Errorf("node_io: Read loc type val TODO")
+		}
+		return loc, nil
+	}
+
+	return nil, fmt.Errorf("node_io: Read unreadable type, loc: %v", loc)
+}
+
+func ReadLocNode(loc *Loc, bufManager BufManager, r io.ReaderAt) (
+	Node, error) {
+	loc, err := loc.Read(bufManager, r)
+	if err != nil {
+		return nil, err
+	}
+
+	if loc.node == nil {
+		return nil, fmt.Errorf("node_io: ReadLocNode, no node")
+	}
+
+	return loc.node, nil
+}
+
+// ----------------------------------------------------
+
 func locateItemLoc(ksl *ItemLoc, key Key,
 	bufManager BufManager, r io.ReaderAt) (*ItemLoc, error) {
 	for ksl != nil {
