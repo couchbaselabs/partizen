b94ddbae8632fbaebc870c3858b9b1d6da28cd63
Steve Yen <steve.yen@gmail.com>
2015-07-02 10:47:25 -0700

    PartitionId/Seq methods for ItemBufRef

--- buf.go
+++ buf.go
@@ -39,6 +39,8 @@ func NewDefaultBufManager(
 	return &DefaultBufManager{arena: arena}
 }
 
+// -------------------------------------------------
+
 func (dbm *DefaultBufManager) Alloc(size int,
 	partUpdater func(cbData, partBuf []byte,
 		partFrom, partTo int) bool, cbData []byte) BufRef {
@@ -53,49 +55,6 @@ func (dbm *DefaultBufManager) Alloc(size int,
 	dbr := &DefaultBufRef{slabLoc}
 
 	return dbr.update(dbm, 0, size, partUpdater, cbData)
-}
-
-// -------------------------------------------------
-
-// Layout of item bytes:
-//   FUTURE_RESERVED(2) + PartitionId(2) +
-//   KeyLen(4) + ValLen(4) + Seq(8) +
-//   KeyBytes + ValBytes.
-const itemFutureReservedBeg = 0
-const itemFutureReservedEnd = 2 // 0 + 2.
-const itemPartitionIdBeg = 2
-const itemPartitionIdEnd = 4 // 2 + 2.
-const itemKeyLenBeg = 4
-const itemKeyLenEnd = 8 // 4 + 4.
-const itemValLenBeg = 8
-const itemValLenEnd = 12 // 8 + 4.
-const itemSeqBeg = 12
-const itemSeqEnd = 20 // 12 + 8.
-const itemHdr = 20
-
-var zeroes8 []byte = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0}
-var zeroes4 []byte = zeroes8[0:4]
-
-func (dbm *DefaultBufManager) AllocItem(keyLen int, valLen int) ItemBufRef {
-	len := itemHdr + keyLen + valLen
-
-	dbm.m.Lock()
-	buf := dbm.arena.Alloc(len)
-	dbm.m.Unlock()
-
-	if buf == nil {
-		return nil
-	}
-
-	// FUTURE_RESERVED + PartitionId.
-	copy(buf[itemFutureReservedBeg:itemPartitionIdEnd], zeroes4)
-
-	binary.BigEndian.PutUint32(buf[itemKeyLenBeg:itemKeyLenEnd], uint32(keyLen))
-	binary.BigEndian.PutUint32(buf[itemValLenBeg:itemValLenEnd], uint32(valLen))
-
-	copy(buf[itemSeqBeg:itemSeqEnd], zeroes8) // Seq.
-
-	return &DefaultBufRef{dbm.arena.BufToLoc(buf)}
 }
 
 // -------------------------------------------------
@@ -194,12 +153,74 @@ func (dbr *DefaultBufRef) Visit(bm BufManager, from, to int,
 
 // -------------------------------------------------
 
+// Layout of item bytes:
+//   FUTURE_RESERVED(2) + PartitionId(2) +
+//   KeyLen(4) + ValLen(4) + Seq(8) +
+//   KeyBytes + ValBytes.
+const itemFutureReservedBeg = 0
+const itemFutureReservedEnd = 2 // 0 + 2.
+const itemPartitionIdBeg = 2
+const itemPartitionIdEnd = 4 // 2 + 2.
+const itemKeyLenBeg = 4
+const itemKeyLenEnd = 8 // 4 + 4.
+const itemValLenBeg = 8
+const itemValLenEnd = 12 // 8 + 4.
+const itemSeqBeg = 12
+const itemSeqEnd = 20 // 12 + 8.
+const itemHdr = 20
+
+var zeroes8 []byte = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0}
+var zeroes4 []byte = zeroes8[0:4]
+
+func (dbm *DefaultBufManager) AllocItem(keyLen int, valLen int) ItemBufRef {
+	len := itemHdr + keyLen + valLen
+
+	dbm.m.Lock()
+	buf := dbm.arena.Alloc(len)
+	dbm.m.Unlock()
+
+	if buf == nil {
+		return nil
+	}
+
+	// FUTURE_RESERVED + PartitionId.
+	copy(buf[itemFutureReservedBeg:itemPartitionIdEnd], zeroes4)
+
+	binary.BigEndian.PutUint32(buf[itemKeyLenBeg:itemKeyLenEnd],
+		uint32(keyLen))
+
+	binary.BigEndian.PutUint32(buf[itemValLenBeg:itemValLenEnd],
+		uint32(valLen))
+
+	copy(buf[itemSeqBeg:itemSeqEnd], zeroes8) // Seq.
+
+	return &DefaultBufRef{dbm.arena.BufToLoc(buf)}
+}
+
+// -------------------------------------------------
+
 func (dbr *DefaultBufRef) PartitionId(bm BufManager) PartitionId {
-	return 0 // TODO.
+	dbm := bm.(*DefaultBufManager)
+
+	dbm.m.Lock()
+	buf := dbm.arena.LocToBuf(dbr.slabLoc)
+	dbm.m.Unlock()
+
+	return PartitionId(binary.BigEndian.Uint16(
+		buf[itemPartitionIdBeg:itemPartitionIdEnd]))
 }
 
 func (dbr *DefaultBufRef) SetPartitionId(bm BufManager,
 	partitionId PartitionId) {
+	dbm := bm.(*DefaultBufManager)
+
+	dbm.m.Lock()
+	buf := dbm.arena.LocToBuf(dbr.slabLoc)
+	dbm.m.Unlock()
+
+	binary.BigEndian.PutUint16(
+		buf[itemPartitionIdBeg:itemPartitionIdEnd],
+		uint16(partitionId))
 }
 
 func (dbr *DefaultBufRef) Key(bm BufManager, out Key) Key {
@@ -211,21 +232,35 @@ func (dbr *DefaultBufRef) KeyLen(bm BufManager) int {
 }
 
 func (dbr *DefaultBufRef) Seq(bm BufManager) Seq {
-	return 0
+	dbm := bm.(*DefaultBufManager)
+
+	dbm.m.Lock()
+	buf := dbm.arena.LocToBuf(dbr.slabLoc)
+	dbm.m.Unlock()
+
+	return Seq(binary.BigEndian.Uint64(buf[itemSeqBeg:itemSeqEnd]))
 }
 
 func (dbr *DefaultBufRef) SetSeq(bm BufManager, seq Seq) {
+	dbm := bm.(*DefaultBufManager)
+
+	dbm.m.Lock()
+	buf := dbm.arena.LocToBuf(dbr.slabLoc)
+	dbm.m.Unlock()
+
+	binary.BigEndian.PutUint64(buf[itemSeqBeg:itemSeqEnd], uint64(seq))
 }
 
 func (dbr *DefaultBufRef) Val(bm BufManager, out Val) Val {
-	return nil
+	return nil // TODO.
 }
 
 func (dbr *DefaultBufRef) ValLen(bm BufManager) int {
-	return 0
+	return 0 // TODO.
 }
 
 func (dbr *DefaultBufRef) ValVisit(bm BufManager, from, to int,
 	partVisitor func(cbData, partBuf []byte,
 		partFrom, partTo int) bool, cbData []byte) {
+	// TODO.
 }
